d766c0b2298ad7809c43b2b2e8804dcf9e05fae4
Jack Tyler <hepavista@gmail.com>
2026-03-31 12:01:23 +0300

    Stream Practice solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int from = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        int to = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1]);
+        return candidate.getClass() == Candidate.class
+                && candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && to - from >= 10;
+    }
 }
